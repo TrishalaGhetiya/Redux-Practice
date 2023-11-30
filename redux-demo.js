f3b2c013c7dc6f6b04348b5788cd83f3de98c4b3
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-11-30 21:21:01 +0530

    increment by 2 and decrement by 2

--- redux-demo.js
+++ redux-demo.js
@@ -3,12 +3,22 @@ const redux = require("redux");
 const counterReducer = (state = { counter: 0 }, action) => {
   if (action.type === "INCREMENT") {
     return {
-      counter: state.counter + 5,
+      counter: state.counter + 1,
     };
   }
   if (action.type === "DECREMENT") {
     return {
       counter: state.counter - 1,
+    };
+  }
+  if (action.type === "INCREMENTBY2") {
+    return {
+      counter: state.counter + 2,
+    };
+  }
+  if (action.type === "DECREMENTBY2") {
+    return {
+      counter: state.counter - 2,
     };
   }
   return state;
@@ -26,3 +36,7 @@ store.subscribe(counterSubscriber);
 store.dispatch({ type: "INCREMENT" });
 
 store.dispatch({ type: "DECREMENT" });
+
+store.dispatch({ type: "INCREMENTBY2" });
+
+store.dispatch({ type: "DECREMENTBY2" });
